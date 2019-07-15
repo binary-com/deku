d194745da6d4ae52c88c68c1b4c077beda62b0f3
Apoorv Jsohi <apoorv@binary.com>
2019-07-15 14:03:54 +0800

    finish services

--- deku/services.py
+++ deku/services.py
@@ -1,9 +1,101 @@
 import docker
+import logging
+import json
+import re
+from time import sleep
 
-
+class Image:
+    def __init__(self, image):
+        match = re.match('(.*):(.*)@(sha256:.*)', image)
+        if match:
+            [name, tag, digest] = match.groups()
+            self.name = name
+            self.tag = tag
+            self.digest = digest
 
 class Services:
     def __init__(self, client):
+        self.client = client
 
-    def list_services(self):
+    def is_docker_running(self):
+        """
+        Check if Docker client is running.
+        """
+        try:
+            return self.client.ping();
+        except docker.errors.APIError as e:
+            logging.warn('Docker service is down: %s' % (e))
+        except:
+            logging.warn('Docker service is down: Unexpected error occured')
+
+        return False
+
+
+
+    def get(self, filters=None):
+        """
+        Get Docker services based on the filter provided, if no filter is passed then all the running services are returned
+        """
+        if self.is_docker_running():
+            return self.client.services.list(filters=filters)
+        return []
+
+    def update(self, **kwargs):
+        """
+        Update docker services 
+
+        Arguments:
+            
+            "filters": Filter services based on id, name, mode or label. Passed as dictionary.
+
+            "update_config": Dictionary of service options updates
+        """
+        service_list = []
+        response = {}
+
+        if 'filters' in kwargs:
+            service_list = self.get(filters=kwargs['filters'])
+        else:
+            # Get all the services
+            service_list = self.get()
+
+        if len(service_list):
+            logging.info('Updating services...')
+        else:
+            logging.info('No services to update')
+            logging.info(json.dumps(kwargs))
+            response['Error'] = 'No services to update'
+
+        completed_services = 0
+        for service in service_list:
+            response[service.name] = {}
+            try:
+                service.update(**kwargs['update_config'])
+                response[service.name]['Status'] = "updating"
+                response[service.name]['Message'] = "Update has been initaited"
+
+            except docker.errors.APIError as e:
+                completed_services += 1
+                logging.warn('Update failed for service %s : %s' %(service.name, e))
+                response[service.name]['Message'] = 'Update failed with error "%s"' %(e)
+                response[service.name]['Status'] = 'failed'
+
+        return response
+
+    def get_status(self, filters=None):
+        """
+        Get last status of service updates
+        """
+        service_list = self.get(filters=filters)
+        response = {}
+        for service in service_list:
+            response[service.name] = {}
+            updateStatus = service.attrs.get('UpdateStatus')
+            if updateStatus:
+                if 'Message' in updateStatus: response[service.name]['Message'] = updateStatus['Message']
+                if 'Image' in updateStatus: response[service.name]['Image'] = service.attrs.get('Spec')['TaskTemplate']['ContainerSpec']['Image']
+                if 'State' in updateStatus: response[service.name]['Status'] = updateStatus['State']
+                if 'StartedAt' in updateStatus:response[service.name]['StartedAt'] = updateStatus['StartedAt']
+
+        return response
 
---
+++ tests/test_services.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+from .context import deku
+import docker
+
+import unittest
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+class ServicesTestSuite(unittest.TestCase):
+    """Test deku services"""
+
+    @mock.patch('deku.services.logging')
+    def test_client(self, logging):
+        dockerClient = mock.Mock()
+        dockerClient.ping.return_value = True
+        logging.warn = mock.Mock()
+
+        # Check if client is running
+        services = deku.services.Services(dockerClient)
+        self.assertTrue(services.is_docker_running)
+
+        # Handle docker api-error exception
+        dockerClient.ping = mock.Mock(side_effect=docker.errors.APIError('Docker client is unavailable'))
+        services.is_docker_running()
+        logging.warn.assert_called_with('Docker service is down: Docker client is unavailable')
+        # Handle all other exceptions
+        dockerClient.ping = mock.Mock(side_effect=Exception())
+        services.is_docker_running()
+        logging.warn.assert_called_with('Docker service is down: Unexpected error occured')
+
+    def test_list_services(self):
+        dockerClient = mock.Mock()
+        dockerClient.ping.return_value = False
+        services = deku.services.Services(dockerClient)
+        # Return empty array when docker is down.
+        self.assertEqual(services.get(), [])
+        dockerClient.ping.return_value = True
+        dockerClient.services.list.return_value = []
+        svcs = services.get()
+        self.assertEqual(svcs, [])
+        dockerClient.services.list.assert_called_with(filters=None)
+
+    def test_update_services_with_no_service_running(self):
+        dockerClient = mock.Mock()
+        dockerClient.ping.return_value = True
+        dockerClient.services = mock.Mock()
+        # When there's no service to update
+        dockerClient.services.list.return_value = []
+        services = deku.services.Services(dockerClient)
+        args = { "filters": {"name":"odin_service"} }
+        resp = services.update(**args)
+        dockerClient.services.list.assert_called_with(filters=args['filters'])
+        self.assertEqual(resp, {'Error': 'No services to update'})
+
+    def test_update_service(self):
+        dockerClient = mock.Mock()
+        dockerClient.ping.return_value = True
+        dockerClient.services = mock.Mock()
+        # When there's a service to update
+        mockService = mock.Mock()
+        mockService.name = 'odin'
+        # when updating service runs into an exception
+        mockService.update = mock.Mock(side_effect=docker.errors.APIError('Some weird error'))
+        dockerClient.services.list.return_value = [mockService]
+        services = deku.services.Services(dockerClient)
+        args = {'update_config': {'image': 'repo/image:tag'}}
+        resp = services.update(**args)
+        self.assertEqual({'odin': {'Message': 'Update failed with error "Some weird error"', 'Status': 'failed'}}, resp)
+        # when service update returns no exception
+        mockService.update = mock.Mock()
+        mockService.attrs.get.return_value = {}
+        resp = services.update(**args)
+        self.assertEqual({'odin': {'Message': 'Update has been initaited', 'Status': 'updating'}}, resp)
+
+    def test_get_update_status(self):
+        dockerClient = mock.Mock()
+        dockerClient.ping.return_value = True
+        dockerClient.services = mock.Mock()
+        # When there's a service to update
+        mockService = mock.Mock()
+        mockService.name = 'thor'
+        service_dict = {'UpdateStatus': {'Message': 'There is always some good in bad', 'Image': 'good', 'State': 'Updating'}, 'Spec': {'TaskTemplate': {'ContainerSpec': {'Image': 'Good'}}}}
+        get_attrs = lambda attr: service_dict[attr]
+        mockService.attrs.get = mock.MagicMock(side_effect=get_attrs)
+        dockerClient.services.list.return_value = [mockService]
+        services = deku.services.Services(dockerClient)
+        resp = services.get_status()
+        self.assertEqual(resp, {'thor': {'Image': 'Good', 'Message': 'There is always some good in bad', 'Status': 'Updating'}})
+        
+if __name__ == '__main__':
+    unittest.main()
